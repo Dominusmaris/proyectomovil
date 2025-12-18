2be73e853f868018fae0d2818b2e455703bdcd69
Evan <evan@duocuc.cl>
2025-12-17 21:43:00 -0300

    Disable Spring Security for open endpoints

--- Backend-SpringBoot/src/main/java/com/duoc/finanzas/Application.java
+++ Backend-SpringBoot/src/main/java/com/duoc/finanzas/Application.java
@@ -2,11 +2,12 @@ package com.duoc.finanzas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 import java.util.HashMap;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
